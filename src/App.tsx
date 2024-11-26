904d11840861c53b1e80900abcf899e9062efb5f
Victor Hugo <118356177+victor17y@users.noreply.github.com>
2024-11-26 07:11:59 -0400

    Victor hugo

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,69 @@
-import { useEffect, useState } from 'react'
+import { useEffect,useState } from "react";
+import {setMusicas,setUsuarios}from './actions';
 import './App.css'
-
-type ProdutoType = {
-  id:number,
-  nome:string,
-  descricao:string,
-  preco:string,
-  imagem:string
+type usuarios ={
+  id: number,
+  nome: string,
+  apellido: string,
+  email: string,
+  password: string,
+  foto:string
 }
-type UsuarioType = {
-  id:number,
-  name:string,
-  email:string,
-  created_at:string,
-  update_at:string 
+type musicas ={
+  id: number,
+  nome_musica: string,
+  cantor_musica: string,
+  genero_musica: string,
+  letra_musica: string,
+  capa_musica : string,
+  lancamento_musica: string,
+  ouvintes_musica: number
 }
-function App() {
-  const [produtos, setProdutos] = useState<ProdutoType[]> ([])
-  const [usuarios, setUsuarios] = useState<UsuarioType[]> ([])
+type album ={
+  id: number,
+  nome_album: string,
+  cantor_album: string,
+  quant_musicas_album: number,
+  lancamento_album: string,
+  capa_album: string,
+  ouvintes_album: number
+}
+const App = () => {
+  const [musicas, setMusicas] = useState<musicas[]>([])
+  const [usuarios, setUsuarios] = useState<usuarios[]>([])
+  const [album, setAlbum] = useState<album[]>([])
+  const [nome, setNome] = useState<string>()
+  const [apellido, setApellido] = useState<string>()
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+}
+useEffect(()=>{
+  fetch("http://localhost:8000/musicas")
+  .then(response => response.json())
+  .then(data => setMusicas(data))
+},[])
+useEffect(()=>{
+  fetch("link do render ")
+  .then(response => response.json())
+  .then(data => setUsuarios(data))
+})
 
-//useEffect(oque fazer e quando fazer)
-  useEffect(()=>{
-    fetch("https://one022b-marketplace-vxxx.onrender.com/produtos")
-    .then(resposta=>resposta.json())
-    .then(dados=>setProdutos(dados))
-  },[])
-  useEffect(()=>{
-    fetch("https://one022b-marketplace-vxxx.onrender.com/usuarios")
-    .then(resposta=>resposta.json())
-    .then(dados=>setUsuarios(dados))
-  },[])
 
-  
+
+/*Listagem de album para um app de musica*/
+useEffect(()=>{
+  fetch("http://localhost:8000/album")
+  .then(response => response.json())
+  .then(data => setAlbum(data))
+  },[])
   return (
-    <>
-    <h2 className="titulo">Produtos</h2>
-    <div className='container-produtos'>
-      {
-        produtos.map(prod=>{
-          return(
-            <div key={prod.id} className='produto-item'>
-              <h1>
-                {prod.nome}
-              </h1>
-              <img src={prod.imagem} alt='Imagem do produto'/>
-              <p>{prod.preco}</p>
-              <p>{prod.descricao}</p>
-            </div>
-          )
-        })
-      }
-       </div>
-       <h2 className="titulo">Usuários</h2>
-       <div className='container-usuarios'>
-      {
-        usuarios.map(user=>{
-          return(
-            <div key={user.id} className='usuario-item'>
-              <h1>{user.name}</h1>
-              <p>{user.email}</p>
-              <p>{user.created_at}</p>
-              <p>{user.update_at}</p>
-            </div>
-          )
-        })
-      }
-       </div>
-       
-    </>
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
   )
-}
 
-export default App
+
+
+
+
