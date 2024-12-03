f182abbedfbf289d3e85b261e8d0b75768ee649d
lucasfelipeoliveira2022 <118355862+lucasfelipeoliveira2022@users.noreply.github.com>
2024-12-03 07:33:25 -0400

    Update CadastroAlbum.tsx

--- src/componentes/cadastro/CadastroAlbum.tsx
+++ src/componentes/cadastro/CadastroAlbum.tsx
@@ -23,7 +23,7 @@ export default function CadastroAlbum(){
             capa_album : capa_album,
             ouvintes_album: ouvintes_album
         }
-        fetch("https://riffly-back.onrender.com/album",{
+        fetch("https://riffly-front.vercel.app/album",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
